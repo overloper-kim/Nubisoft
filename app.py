1411509f9c22b48d33484d8c39769749da45eb38
cshrevolution <seokhoon77@naver.com>
2025-01-16 23:42:28 +0900

    second commit csh

--- app.py
+++ app.py
@@ -29,12 +29,15 @@ def post_home():
   print("post 요청")
   return "끝"
 
-@app.route("/login", methods=['POST'], methods=['GET'])
+@app.route("/login", methods=['GET'])
 def get_login():
   component_name = 'login'
+  return render_template('index.html', context=component_name)
 
-  emailReceive = request.form['emailGive']
-  passwordReceive = request.form['passwordGive']
+@app.route("/login", methods=['POST'])
+def post_login():
+  email = request.form['email']
+  pw = request.form['pw']
 
   cur.execute('SELECT * FROM User;')
   data = cur.fetchall()
@@ -42,28 +45,21 @@ def get_login():
   result.append([])
   result[0].append('userID')
   result[0].append('userEmail')
+  result[0].append('userPassword')
   
   count = 1
 
   for rowdata in data:
       result.append([])
-      result[count].append(rowdata[0])
       result[count].append(rowdata[1])
+      result[count].append(rowdata[2])
       count += 1
 
-  for i in len(result):
-    if (emailReceive in result[i] and passwordReceive == result[i][2]):
+  for i in range(len(result)):
+    if (email in result[i][0] and pw == result[i][1]):
       print("로그인 성공")
       break
 
-  return render_template('index.html', context=component_name)
-
-@app.route("/login", methods=['POST'])
-def post_login():
-  email = request.form['email']
-  pw = request.form['pw']
-  print(email)
-  print(pw)
   return "post 요청"
 
 @app.route("/signup")
@@ -74,27 +70,6 @@ def signup():
 @app.route("/game")
 def game():
   component_name = 'game'
-  return render_template('index.html', context=component_name)
-
-@app.route("/signup", methods=['POST'])
-def signup():
-  component_name = 'signup'
-
-  emailReceive = request.form['emailGive']
-  passwordReceive = request.form['passwordGive']
-  print(emailReceive, passwordReceive)
-
-  return render_template('index.html', context=component_name)
-
-@app.route("/modifyuser")
-def modifyuser():
-  component_name = 'signup'
-
-  emailReceive = request.form['emailGive']
-  passwordReceive = request.form['passwordGive']
-
-  cur.execute()
-
   return render_template('index.html', context=component_name)
 
 if __name__ == '__main__':
